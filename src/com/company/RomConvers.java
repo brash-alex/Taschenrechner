88c777b29128b8b6dbf666ad4dbe2da7984db42f
brash-alex <64625137+brash-alex@users.noreply.github.com>
2020-05-05 20:02:09 +1000

    Update RomConvers.java

--- src/com/company/RomConvers.java
+++ src/com/company/RomConvers.java
@@ -50,7 +50,7 @@ enum RomanNumeral {
         }
 
         if (romanNumeral.length() > 0) {
-            System.out.println("\""+ input+ "\"" + " не полуилсь сконвертировать, точно это Римские цифры?");
+            System.out.println("\""+ input+ "\"" + " не получилсь сконвертировать, точно это Римские цифры?");
             result = -999999;
         }
 
